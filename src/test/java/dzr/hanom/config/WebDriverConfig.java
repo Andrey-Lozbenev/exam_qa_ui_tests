9df18ca9a656aba9f42e1a1d5c143bb9801ffcd4
Andrey <hanom_dzr@mail.ru>
2023-06-14 15:20:44 +0300

    add ENV

--- src/test/java/dzr/hanom/config/WebDriverConfig.java
+++ src/test/java/dzr/hanom/config/WebDriverConfig.java
@@ -3,7 +3,7 @@ package dzr.hanom.config;
 import org.aeonbits.owner.Config;
 
 @Config.Sources({
-        "classpath:${env}.properties"
+        "classpath:${ENV}.properties"
 })
 public interface WebDriverConfig extends Config {
 
