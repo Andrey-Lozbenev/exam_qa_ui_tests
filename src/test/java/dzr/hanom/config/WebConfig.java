9675272399cef089388450d49213840832e769a9
Andrey <hanom_dzr@mail.ru>
2023-06-15 13:59:42 +0300

    add ProjectConfiguration fix

--- src/test/java/dzr/hanom/config/WebConfig.java
+++ src/test/java/dzr/hanom/config/WebConfig.java
@@ -11,6 +11,7 @@ import org.aeonbits.owner.Config;
 })
 public interface WebConfig extends Config {
     @Key("browser")
+    @DefaultValue("chrome")
     Browser browser();
     @Key("browserVersion")
     String browserVersion();
